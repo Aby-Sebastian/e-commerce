80b195a11b61c8d18aa86516969d24e894d61898
Aby Sebastian <abysebastian98@gmail.com>
2022-09-25 14:10:12 +0530

    added tests to accounts app

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def test_login(self):
+        response = self.client.get('/accounts/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout(self):
+        response = self.client.get('/accounts/logout/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_register(self):
+        response = self.client.get('/accounts/register/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_reset_password(self):
+        response = self.client.get('/accounts/reset_password/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_reset_password_sent(self):
+        response = self.client.get('/accounts/reset_password_sent/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_reset_password_complete(self):
+        response = self.client.get('/accounts/reset_password_complete/')
+        self.assertEqual(response.status_code, 200)
--- project/urls.py
+++ project/urls.py
@@ -19,7 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('app.urls'))
+    path('',include('app.urls')),
+    path('accounts/',include('accounts.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
